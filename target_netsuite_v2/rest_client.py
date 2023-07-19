63e08a9cc91be1cda1856a323fa779fba43dfaaf
Anthony Ramos <anthony.ramos@hotglue.io>
2023-07-19 14:34:25 +0000

    Revert "made changes to items stream, and made stream finding case-insensitive"
    This reverts commit 03b422247bf6c2987f029c2d1437c81788c6f7d6

--- target_netsuite_v2/rest_client.py
+++ target_netsuite_v2/rest_client.py
@@ -684,10 +684,10 @@ class netsuiteRestV2Sink(BatchSink):
             'displayName': record.get('name'),
             'createdAt': record.get('createdAt'),
             'reorderPoint': record.get('reorderPoint'), 
-            'upcCode':record.get('code'),
+            'upcCode':record.get('sku'),
             'quantityOnHand': record.get('quantityOnHand'), 
             'isInactive': not record.get('active'), 
-            'itemId': record.get('name'),
+            'itemId': record.get('code'),
         }
         
 
--- target_netsuite_v2/sinks.py
+++ target_netsuite_v2/sinks.py
@@ -34,36 +34,36 @@ class netsuiteV2Sink(netsuiteSoapV2Sink, netsuiteRestV2Sink):
         if self.stream_name.lower() in ["customer"]:
             customer = self.process_customer(context,record)
             context['Customer'].append(customer)
-        if self.stream_name.lower() in ["inboundshipment","inboundshipments"]:
+        if self.stream_name=="InboundShipment":
             inbound_shipment = self.process_inbound_shipment(context, record)
             context["InboundShipment"].append(inbound_shipment)
-        elif self.stream_name.lower() in ["customerpayment","customerpayments"]:
+        elif self.stream_name=="CustomerPayment":
             customer_payment = self.process_customer_payment(context, record)
             context["CustomerPayment"].append(customer_payment)
-        elif self.stream_name.lower() in ["salesorder","salesorders"]:
+        elif self.stream_name=="SalesOrder":
             sale_order = self.process_order(context, record)
             context["SalesOrder"].append(sale_order)
         elif self.stream_name.lower() in ["invoice", "invoices"]:
             invoice = self.process_invoice(context, record)
             context["Invoice"].append(invoice)
-        elif self.stream_name.lower() in ["creditmemo","creditmemos"]:
+        elif self.stream_name == "CreditMemo":
             credit_memo = self.process_credit_memo(context, record)
             context["CreditMemo"].append(credit_memo)
-        elif self.stream_name.lower() in ["vendor","vendors"]:
+        elif self.stream_name == "Vendor":
             vendor = self.process_vendors(context, record)
             context["Vendor"].append(vendor)
-        elif self.stream_name.lower() in ["vendorbill", "vendorbills", "purchaseinvoices","purchaseinvoice", "bill", "bills"]:
+        elif self.stream_name in ["vendorBill", "VendorBill", "PurchaseInvoices", "Bill", "Bills", "bill", "bills"]:
             vendor_bill = self.process_vendor_bill(context, record)
             context["vendorBill"].append(vendor_bill)
-        elif self.stream_name.lower() in ["invoicepayments","invoicepayment"]:
+        elif self.stream_name=="InvoicePayment":
             invoice_payment = self.invoice_payment(context, record)
             context["InvoicePayment"].append(invoice_payment)
-        elif self.stream_name.lower() in ["vendorpayments","vendorpayment"]:
+        elif self.stream_name=="VendorPayment":
             vendor_payment = self.vendor_payment(context, record)
             context["VendorPayment"].append(vendor_payment)
-        elif self.stream_name.lower() in ["purchaseordertovendorbill"]:
+        elif self.stream_name=="PurchaseOrderToVendorBill":
             context["PurchaseOrderToVendorBill"].append(record)
-        elif self.stream_name.lower() in ['item','items']:
+        elif self.stream_name == 'Items':
             item = self.process_item(context,record)
             context['Items'].append(item)
 
@@ -72,7 +72,7 @@ class netsuiteV2Sink(netsuiteSoapV2Sink, netsuiteRestV2Sink):
     def process_batch(self, context: dict) -> None:
         """Write out any prepped records and return once fully written."""
         self.logger.info(f"Posting data for entity {self.stream_name}")
-        if self.stream_name.lower() in ["journalentries", "journalentry", "customerpayment"]:
+        if self.stream_name.lower() in ["journalentries", "journalentry", "customerpayment", "inboundshipment"]:
             if self.stream_name.lower() in ["journalentries", "journalentry"]:
                 name = "JournalEntry"
             else:
@@ -80,7 +80,7 @@ class netsuiteV2Sink(netsuiteSoapV2Sink, netsuiteRestV2Sink):
             for record in context.get(name, []):
                 response = self.ns_client.entities[name].post(record)
                 self.logger.info(response)
-        elif self.stream_name.lower() in ["salesorder","salesorders"]:
+        elif self.stream_name in ["SalesOrder"]:
             url = f"{self.url_base}salesOrder"
             for record in context.get(self.stream_name, []):
                 if record.get("order_number") is None:
@@ -92,49 +92,43 @@ class netsuiteV2Sink(netsuiteSoapV2Sink, netsuiteRestV2Sink):
             url = f"{self.url_base}invoice"
             for record in context.get("Invoice", []):
                 response = self.rest_post(url=url, json=record)
-        elif self.stream_name.lower() in ["creditmemo","creditmemos"]:   
+        elif self.stream_name in ["CreditMemo"]:   
             endpoint = self.stream_name.lower()
             url = f"{self.url_base}{endpoint}"
             for record in context.get(self.stream_name, []):
                 response = self.rest_post(url=url, json=record)
-        elif self.stream_name.lower() in ["vendor","vendors"]:   
+        elif self.stream_name in ["Vendor"]:   
             endpoint = self.stream_name.lower()
             url = f"{self.url_base}{endpoint}"
             for record in context.get(self.stream_name, []):
                 response = self.rest_post(url=url, json=record)
-        elif self.stream_name.lower() in ["vendorbill","vendorbills","bill","bills"]:
+        elif self.stream_name in ["vendorBill", "VendorBill", "PurchaseInvoices", "Bill", "Bills", "bill", "bills"]:
             url = f"{self.url_base}vendorbill"
             for record in context.get("vendorBill", []):
                 response = self.rest_post(url=url, json=record)
-        elif self.stream_name.lower() in ["invoicepayment","invoicepayments"]:
+        elif self.stream_name in ["InvoicePayment"]:
             for record in context.get(self.stream_name, []):
                 response = self.push_payments(record)
-        elif self.stream_name.lower() in ["vendorpayment","vendorpayments"]:
+        elif self.stream_name in ["VendorPayment"]:
             for record in context.get(self.stream_name, []):
                 response = self.push_vendor_payments(record)
-        elif self.stream_name.lower() in ["purchaseordertovendorbill"]:
+        elif self.stream_name in ["PurchaseOrderToVendorBill"]:
             for record in context.get(self.stream_name, []):
                 response = self.po_to_vb(record)
-        elif self.stream_name.lower() in ['inboundshipment','inboundshipments']:
+        elif self.stream_name in ["InboundShipment"]:
+            endpoint = list(self.stream_name)
+            endpoint[0] = endpoint[0].lower()
+            endpoint = "".join(endpoint)
+            endpoint = endpoint + "/{id}"
             for record in context.get(self.stream_name, []):
-                if record.get("id"):
-                    endpoint = list(self.stream_name)
-                    endpoint[0] = endpoint[0].lower()
-                    endpoint = "".join(endpoint)
-                    endpoint = endpoint + "/{id}"
-                    endpoint = endpoint.format(id=record.pop("id"))
-                    url = f"{self.url_base}{endpoint}"
-                    response = self.rest_patch(url=url, json=record)
-                else: 
-                    response = self.ns_client.entities[name].post(record)
-                
-                self.logger.info(response)
-    
-        elif self.stream_name.lower() in ['customers','customer']:
+                endpoint = endpoint.format(id=record.pop("id"))
+                url = f"{self.url_base}{endpoint}"
+                response = self.rest_patch(url=url, json=record)
+        elif self.stream_name in ['Customer']:
             url = f"{self.url_base}{self.stream_name.lower()}"
             for record in context.get(self.stream_name, []):
                 response = self.rest_post(url=url, json=record)
-        elif self.stream_name.lower() in ['item','items']:
+        elif self.stream_name in ['Item','Items']:
             url = f"{self.url_base}inventoryItem"
             for record in context.get(self.stream_name,[]):
                 response = self.rest_post(url=url,json=record)
