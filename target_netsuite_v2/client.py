d2b5d652b74a7722373b453efc7a632e42cefbbe
Nassredean Nasseri <nassredean@gmail.com>
2025-02-10 15:47:02 -0500

    Added NetSuiteBatchSink class

--- target_netsuite_v2/client.py
+++ target_netsuite_v2/client.py
@@ -76,3 +76,56 @@ class NetSuiteSink(NetSuiteBaseSink, HotglueSink):
         self.validate_response(response)
         id = self._extract_id_from_response_header(response.headers)
         return id, response.ok, dict()
+
+class NetSuiteBatchSink(NetSuiteBaseSink, HotglueBatchSink):
+    max_size = 1
+
+    # process_batch_record is a HotglueBatchSync expected override
+    # --> this is where we do the mapping
+
+    # handle_batch_response is a HotglueBatchSync expected override
+    # --> this is where we transform results into a state_updates dictionary
+
+    def process_batch(self, context: dict) -> None:
+        if not self.latest_state:
+            self.init_state()
+
+        raw_records = context["records"]
+
+        records = list(map(lambda e: self.process_batch_record(e[1], e[0]), enumerate(raw_records)))
+
+        results = self.make_batch_request(records)
+
+        state_updates = self.handle_batch_response(results)
+
+        for state in state_updates.get("state_updates", list()):
+            self.update_state(state)
+
+    # HotglueBatchSync expected override
+    def make_batch_request(self, records: List[Dict]):
+        results = []
+        for record in records:
+            id, success, state = self.upsert_record(record, {})
+            results.append({"id": id, "success": success, "state": state})
+        return results
+
+    def upsert_record(self, record: dict, context: dict):
+        if self.record_exists(record, context):
+            response = self.request_api("PATCH", request_data=record, endpoint=f"{self.endpoint}/{record['internalId']}")
+        else:
+            response = self.request_api("POST", request_data=record)
+        success, error_message = self.validate_response(response)
+        id = self._extract_id_from_response_header(response.headers)
+        return id, success, dict()
+
+    def validate_response(self, response: requests.Response) -> tuple[bool, str | None]:
+        """Validate HTTP response.
+
+        Returns:
+            tuple[bool, str | None]: Returns (True, None) if successful, (False, error_message) if validation fails
+        """
+        if response.status_code >= 400:
+            msg = self.response_error_message(response)
+            return False, msg
+        else:
+            return True, None
