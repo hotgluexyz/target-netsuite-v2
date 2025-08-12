a634c08cf873c38651b21a9d7b2a833226a463f6
Vinicius Ghellere Sbardelotto <vinicius@hotglue.io>
2025-08-12 14:52:31 -0300

    feat: add TaxCodes to reference_data

--- target_netsuite_v2/suite_talk_client.py
+++ target_netsuite_v2/suite_talk_client.py
@@ -20,7 +20,8 @@ class SuiteTalkRestClient:
         "customercategory": "customercategory.id as internalid, customercategory.externalid as externalid, customercategory.name",
         "vendorcategory": "vendorcategory.id as internalId, vendorcategory.externalId as externalId, vendorcategory.name",
         "employee": "employee.id as internalid, employee.externalId as externalid, employee.firstname || ' ' || employee.lastname AS name, subsidiary as subsidiaryId",
-        "item": "item.id as internalid, item.externalId as externalId, item.fullName as name, item.itemid as itemId"
+        "item": "item.id as internalid, item.externalId as externalId, item.fullName as name, item.itemid as itemId",
+        "salestaxitem": "salestaxitem.id as internalid, salestaxitem.name as name"
     }
 
     ref_name_where_clauses = {
