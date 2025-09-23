32f68c541da19b96085bc99ac69c843082b9149a
Vinicius Ghellere Sbardelotto <vinicius@hotglue.io>
2025-09-23 17:26:13 -0300

    feat: map taxes for vendorCredit items and expenses

--- target_netsuite_v2/mapper/vendor_credit_schema_mapper.py
+++ target_netsuite_v2/mapper/vendor_credit_schema_mapper.py
@@ -41,6 +41,7 @@ class VendorCreditSchemaMapper(BaseMapper):
             **self._map_custom_fields()
         }
 
+        self._map_tax_details(payload)
         self._map_fields(payload)
 
         return payload
@@ -62,25 +63,46 @@ class VendorCreditSchemaMapper(BaseMapper):
     def _map_line_items(self, subsidiary_id):
         line_items = self.record.get("lineItems", [])
         mapped_line_items = []
+        mapped_tax_lines = []
 
-        for line_item in line_items:
+        for index, line_item in enumerate(line_items):
             payload = VendorCreditLineItemSchemaMapper(line_item, self.reference_data, subsidiary_id).to_netsuite()
+            if tax_code := line_item.get("taxCode"):
+                tax_details_reference = f"NEW_ITEM_{index}"
+                payload["taxDetailsReference"] = tax_details_reference
+                tax_line = self._map_tax_line(tax_details_reference, tax_code, line_item.get("amount"), line_item.get("taxAmount"))
+                mapped_tax_lines.append(tax_line)
             mapped_line_items.append(payload)
 
         if mapped_line_items:
-            return { "item": { "items": mapped_line_items } }
+            return { "item": { "items": mapped_line_items }, "itemsTaxDetails": mapped_tax_lines }
         else:
             return {}
         
     def _map_expense_lines(self, subsidiary_id):
         expense_lines = self.record.get("expenses", [])
         mapped_expense_lines = []
+        mapped_tax_lines = []
 
-        for expense_line in expense_lines:
+        for index, expense_line in enumerate(expense_lines):
             payload = VendorCreditExpenseLineSchemaMapper(expense_line, self.reference_data, subsidiary_id).to_netsuite()
+            if tax_code := expense_line.get("taxCode"):
+                tax_details_reference = f"NEW_EXPENSE_{index}"
+                payload["taxDetailsReference"] = tax_details_reference
+                tax_line = self._map_tax_line(tax_details_reference, tax_code, expense_line.get("amount"), expense_line.get("taxAmount"))
+                mapped_tax_lines.append(tax_line)
             mapped_expense_lines.append(payload)
 
         if mapped_expense_lines:
-            return { "expense": { "items": mapped_expense_lines } }
+            return { "expense": { "items": mapped_expense_lines }, "expensesTaxDetails": mapped_tax_lines }
         else:
             return {}
+    
+    def _map_tax_details(self, payload):
+        items_tax_details = payload.pop("itemsTaxDetails", [])
+        expenses_tax_details = payload.pop("expensesTaxDetails", [])
+        tax_details = items_tax_details + expenses_tax_details
+
+        if tax_details:
+            payload["taxDetailsOverride"] = True
+            payload["taxDetails"] = { "items": tax_details }
