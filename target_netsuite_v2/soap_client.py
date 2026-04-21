51384b9184e69606b906e284b89d5b949c4a5e48
Keyna Rafael <95432445+keyn4@users.noreply.github.com>
2026-04-21 15:15:37 -0500

    HGI-10189: Add body_fields_only param for accounts reference data (#67)

--- target_netsuite_v2/soap_client.py
+++ target_netsuite_v2/soap_client.py
@@ -148,7 +148,12 @@ class netsuiteSoapV2Sink(HotglueSink):
         try:
             reference_data["Accounts"] = self.ns_client.entities["Accounts"](self.ns_client.ns_client).get_all(["acctName", "acctNumber", "subsidiaryList", "acctType", "class", "department", "isInactive", "location"], page_size=100)
         except Exception as e:
-            self._check_exception(e, "Accounts")
+            if "You need  the 'Lists -> Documents and Files' permission" in str(e):
+                self.logger.info(f"Permissions for Documents and Files missing. Attempting to get Accounts with body_fields_only=True")
+                reference_data["Accounts"] = self.ns_client.entities["Accounts"](self.ns_client.ns_client, body_fields_only=True).get_all(["acctName", "acctNumber", "subsidiaryList", "acctType", "class", "department", "isInactive", "location"], page_size=100)
+                self.ns_client.ns_client._search_preferences.bodyFieldsOnly = False
+            else:
+                self._check_exception(e, "Accounts")
 
         try:
             reference_data["Locations"] = self.ns_client.entities["Locations"].get_all(["name", "subsidiaryList", "isInactive"], page_size=100)
